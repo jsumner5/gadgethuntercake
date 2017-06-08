5761f2ebdef12c2fcc768b50914bd68c8ae80fba
Jerold Sumner <jeroldsumner@gmail.com>
2017-06-08 17:08:49 -0400

    fix database config for remote

--- config/app.php
+++ config/app.php
@@ -263,7 +263,7 @@ return [
         /**
          * The test connection is used during the test suite.
          */
-        'gh' => [
+        'default' => [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
@@ -280,7 +280,7 @@ return [
             //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
             'url' => env('DATABASE_TEST_URL', null),
         ],
-        'default' => [
+        'notdefault' => [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
