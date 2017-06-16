14e1da9690254daa58e9e36d5fa1829a5218a63d
Jerold Sumner <jeroldsumner@gmail.com>
2017-06-16 15:02:04 -0400

    testing

---
+++ src/Shell/HelloShell.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jerold
+ * Date: 6/10/2017
+ * Time: 5:09 PM
+ */
+
+namespace App\Shell;
+
+use Cake\Console\Shell;
+
+class HelloShell extends Shell
+{
+    public function main()
+    {
+        include_once '../Controller/Component/AmazonService/AmazonService.php';
+        $this->out('Hello world.');
+    }
+}
